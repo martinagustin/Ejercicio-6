70dcac9e241734c810d87b1e08b91e95a0c519b9
Tin Garcia <Martin Garcia>
2022-05-13 15:08:48 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio-6/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ 1) Generar un número secreto
+aleatorio con la siguiente instruccion:
+
+// Genera un numero entero aleatorio de 1 a 20
+int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+2) Pedir al usuario que ingrese un número y que
+intente adivinar el número que eligió la computadora.
+
+3) Si el numero ingresado por el usuario es mayor
+al número secreto, avisarle que es muy grande y que intente de nuevo y que
+vuelva al paso 2.
+
+4) Si el numero ingresado es menor al número
+secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
+
+5) Si el número ingresado coincide con el número
+secreto, sacar el siguiente mensaje:
+
+"Felicitaciones, has adivinado el número
+secreto que era:  [numeroSecreto]"
+"Lo has logrado en [n] intentos!!"
+
+Y finalizar el programa.
+ */
+int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int numeroIngresado = 0;
+int contador = 0;
+while (numeroIngresado != numeroSecreto)
+{
+    Console.WriteLine(" Ingrese un numero");
+    numeroIngresado = Convert.ToInt32(Console.ReadLine());
+    contador++;
+    if (numeroIngresado > numeroSecreto)
+    {
+        Console.WriteLine("numero ingresado muy grande, intentalo de nuevo");
+    }
+    else if (numeroIngresado < numeroSecreto)
+    {
+        Console.WriteLine("numero ingresado muy chico, intentalo de nuevo");
+    }
+}
+Console.WriteLine($"Felicitaciones el numero secreto era: {numeroSecreto}, lo has logrado en {contador} intentos");
